9dad7fcadfd5533c92d6cc3b0d54d9f5bdc3b10a
Jan Olszak <jan.olszak@vestiacom.com>
2017-06-07 17:25:02 +0200

    Main header for the library
    Change-Id: I79482d1d81fa8c47e98906ac9e46c45e6b946d59

---
+++ src/everest.hpp
@@ -0,0 +1,12 @@
+#ifndef EVEREST_EVEREST_HPP_
+#define EVEREST_EVEREST_HPP_
+
+// Helper header with all other headers needed to setup libeverest server.
+
+#include "types.hpp"
+#include "server.hpp"
+#include "request.hpp"
+#include "response.hpp"
+
+
+#endif // EVEREST_EVEREST_HPP_
